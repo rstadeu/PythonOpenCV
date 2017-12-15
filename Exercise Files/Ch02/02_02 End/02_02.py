f2c580800424b009c7fc7f5ef145f8e4953e471d
rstadeu <rstadeu@yahoo.com.br>
2017-12-15 18:27:55 +0100

    My Own Files

--- Exercise Files/Ch02/02_02 End/02_02.py
+++ Exercise Files/Ch02/02_02 End/02_02.py
@@ -1,8 +1,7 @@
->>> import numpy as np
->>> import cv2
->>> img = cv2.imread("opencv-logo.png", 1)
->>> 
->>> img
+import numpy as np
+import cv2
+img = cv2.imread("opencv-logo.png", 1)
+
 array([[[255, 255, 255],
         [255, 255, 255],
         [255, 255, 255],
@@ -51,23 +50,23 @@ array([[[255, 255, 255],
         [255, 255, 255],
         [255, 255, 255],
         [255, 255, 255]]], dtype=uint8)
->>> type(img)
+ type(img)
 <class 'numpy.ndarray'>
->>> len(img)
+ len(img)
 739
->>> len(img[0])
+ len(img[0])
 600
->>> len(img[0][0])
+ len(img[0][0])
 3
->>> img.shape
+ img.shape
 (739, 600, 3)
->>> img.dtype
+ img.dtype
 dtype('uint8')
->>> 2**8
+ 2**8
 256
->>> img[10, 5]
+ img[10, 5]
 array([255, 255, 255], dtype=uint8)
->>> img[:, :, 0]
+ img[:, :, 0]
 array([[255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
@@ -75,5 +74,5 @@ array([[255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255]], dtype=uint8)
->>> img.size
+ img.size
 1330200
