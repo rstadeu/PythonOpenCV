9a7803b9bdbfa06341294fd86338413278c0c0c6
rstadeu <rstadeu@yahoo.com.br>
2017-12-15 21:39:36 +0100

    Changing the value of the filters

--- MyExercises/ObjectDetection/AdaptativeThreshold.py
+++ MyExercises/ObjectDetection/AdaptativeThreshold.py
@@ -7,7 +7,7 @@ cv2.imshow("Original Image", img)
 ret, thresh_basic = cv2.threshold(img, 70, 255, cv2.THRESH_BINARY)
 cv2.imshow("Basic Binary", thresh_basic)
 
-thres_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
+thres_adapt = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
 cv2.imshow("Adaptive Threshold", thres_adapt)
 
