c4ca95e80429b5a057af00f84fd97ffaf66e1c0c
rstadeu <rstadeu@yahoo.com.br>
2017-12-16 11:31:04 +0100

    Editing ContourObjectDetection.py

--- MyExercises/ObjectDetection/ContourObjectDetection.py
+++ MyExercises/ObjectDetection/ContourObjectDetection.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 img = cv2.imread("detect_blob.png",1)
-img = cv2.resize(img,(0,0), fx=0.8, fy=0.8)
+#img = cv2.resize(img,(0,0), fx=0.8, fy=0.8)
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,115,1)
 
@@ -16,7 +16,23 @@ img2 = img.copy()
 index = -1
 thickness = 2
 color = (255,125,0)
-cv2.drawContours(img2, contours, index, color, thickness)
-cv2.imshow("Contours", img2)
+
+objects = np.zeros([img.shape[0], img.shape[1],3], 'uint8')
+for c in contours:
+    cv2.drawContours(objects, [c], -1, color, -1)
+
+    area = cv2.contourArea(c)
+    perimeter = cv2.arcLength(c, True)
+    M = cv2.moments(c)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+
+    cv2.circle(objects, (cx,cy), 4, (0,0,255), -1)
+
+    print("Area: {}, perimeter: {}".format(area, perimeter))
+
+    cv2.imshow("Contours", objects)
+
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
